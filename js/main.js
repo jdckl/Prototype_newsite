0a4434e5381e1fd112e5b86eb8dcfafe9653b556
Jan Dočekal <honza.doc@gmail.com>
2016-03-22 13:27:50 +0100

    Test (perlinoise)

--- js/main.js
+++ js/main.js
@@ -1,79 +1,67 @@
 $(document).ready(function() {
-var vertexHeight = 15;
-var planeDefinition = 50;
-var planeSize = 20000;
+var renderer	= new THREE.WebGLRenderer({
+		antialias	: true
+	});
+	renderer.setSize( window.innerWidth, window.innerHeight );
 
-var container = document.getElementById('head_threejs');
-var fog = new THREE.Fog(0x000000, 300, 7000);
-var camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight,1, 20000)
+  var container = document.getElementById('head_threejs');
+	container.appendChild( renderer.domElement );
 
-camera.position.z = 3500;
-
-var scene = new THREE.Scene();
-
-var geometry = new THREE.PlaneGeometry(planeSize, planeSize, planeDefinition, planeDefinition);
-  geometry.dynamic = true;
-  geometry.computeFaceNormals();
-  geometry.computeVertexNormals();
-  geometry.normalsNeedUpdate = true;
-
-var material = new THREE.MeshPhongMaterial({color: 0xffffff, specular: 0xffffff, shininess: 0, shading: THREE.FlatShading});
-
-var	plane = new THREE.Mesh(geometry, material);
-plane.rotation.x -=1.1;
-
-scene.add( plane );
-scene.fog = fog;
-
-  var ambientLight = new THREE.AmbientLight(0x1a1a1a);
-  scene.add( ambientLight );
-
-  var dirLight = new THREE.DirectionalLight(0xdfe8ef, 0.3);
-  dirLight.position.set(3, 5, 1);
-  scene.add( dirLight );
+	var onRenderFcts= [];
+	var scene	= new THREE.Scene();
+	var camera	= new THREE.PerspectiveCamera(25, window.innerWidth /    window.innerHeight, 0.01, 1000);
 
 
-var renderer = new THREE.WebGLRenderer({antialiasing: true, alpha: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-container.appendChild( renderer.domElement );
+	camera.position.z = 15;
+   camera.position.y = 2;
 
-updatePlane();
+  scene.fog = new THREE.Fog(0x000, 0, 45);
+	;(function(){
 
- function updatePlane() {
-   for (var i = 0; i < plane.geometry.vertices.length; i++)
-   {
-     plane.geometry.vertices[i].z += Math.random()*vertexHeight -vertexHeight;
-   }
- };
+		var light	= new THREE.AmbientLight( 0x202020 )
+		scene.add( light )
 
- updateVerts();
+		var light	= new THREE.DirectionalLight('white', 5)
+		light.position.set(0.5, 0.0, 2)
+		scene.add( light )
 
+		var light	= new THREE.DirectionalLight('white', 0.75*2)
+		light.position.set(-0.5, -0.5, -2)
+		scene.add( light )
+	})()
+	var heightMap	= THREEx.Terrain.allocateHeightMap(256,256)
+	THREEx.Terrain.simplexHeightMap(heightMap)
+	var geometry	= THREEx.Terrain.heightMapToPlaneGeometry(heightMap)
+	THREEx.Terrain.heightMapToVertexColor(heightMap, geometry)
 
- function updateVerts() {
-   for (var i = 0; i < plane.geometry.vertices.length; i++)
-   {
-     plane.geometry.vertices[i].z -= Math.random()*vertexHeight -(vertexHeight/2);
-   }
-   plane.geometry.verticesNeedUpdate = true;
-   plane.geometry.normalsNeedUpdate = true;
- };
+	var material	= new THREE.MeshBasicMaterial({
+		wireframe: true
+	});
 
-render();
+  var mesh	= new THREE.Mesh( geometry, material );
+	scene.add( mesh );
 
-			function render() {
-        requestAnimationFrame( render );
-        updateVerts();
-        renderer.render( scene, camera );
-			}
+  mesh.lookAt(new THREE.Vector3(0,1,0));
+	mesh.scale.y	= 4;
+	mesh.scale.x	= 3;
+	mesh.scale.z	= 0.20;
+	mesh.scale.multiplyScalar(10);
 
+	onRenderFcts.push(function(delta, now){
+		mesh.rotation.z += 0.05 * delta;
+	})
+	onRenderFcts.push(function(){
+		renderer.render( scene, camera );
+	})
 
-      window.addEventListener( 'resize', onWindowResize, false );
-
-function onWindowResize() {
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-			}
+	var lastTimeMsec= null
+	requestAnimationFrame(function animate(nowMsec){
+		requestAnimationFrame( animate );
+		lastTimeMsec	= lastTimeMsec || nowMsec-1000/60
+		var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
+		lastTimeMsec	= nowMsec
+		onRenderFcts.forEach(function(onRenderFct){
+			onRenderFct(deltaMsec/1000, nowMsec/1000)
+		})
+	})
 });
